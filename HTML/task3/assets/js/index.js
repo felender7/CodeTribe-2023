b022d6c2dc9a6f7289bf8832e79d7e4d86dc9696
Felender <felender7@gmail.com>
2023-02-20 20:49:28 +0200

    updated index page

--- HTML/task3/assets/js/index.js
+++ HTML/task3/assets/js/index.js
@@ -4,8 +4,11 @@ window.onload = function () {
     let firstName = 'Felender';
     let postion = 'And Im a Front-end // Back-end Developer';
     let dot = "<span class='small-box blink'>.</span>";
+
     document.getElementById("hero-title").innerHTML = 'I am ' + firstName + dot;
     document.getElementById("hero-subtitle").innerHTML = postion;
+    document.getElementById("hero-mini-title").innerHTML = "I Design & Build Creative Products";
+    document.getElementById("hero-btn").innerHTML = "<a href='contacts.html' class='btn btn-success btn-lg'>HIRE ME</a>";
 
 
     //BIO SECTION - HOME PAGE
@@ -22,10 +25,14 @@ window.onload = function () {
     document.getElementById("bio-subtitle").innerHTML = bioSubTitle;
     document.getElementById("bio-about").innerHTML = boiAbout;
     document.getElementById("boi-image").src = "/assets/img/illustrations/bio-avator.png";
+    document.getElementById("button").innerHTML ="<a href='contacts.html' class='btn btn-success mt-2'>HIRE ME</a> " + " <a href='assets/downloads/Tlangelani_Resume.pdf' target='_blank' class='btn btn-secondary mt-2'>DOWNLOAD CV</a> ";
+
+
+    
+    
 
 
     //EDUCATION SECTION - ABOUT PAGE
-
     //Quilifications
     let QuilificationsList = new Array(
         {
