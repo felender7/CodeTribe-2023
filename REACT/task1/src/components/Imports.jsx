57d5db86beb463c610242a348b16749bd15c133c
felender7 <felender7@gmail.com>
2023-06-06 16:38:44 +0200

    added readme

--- REACT/task1/src/components/Imports.jsx
+++ REACT/task1/src/components/Imports.jsx
@@ -2,11 +2,13 @@ import appStoreBadge from '../assets/app-store-badge.svg';
 import googlePlayBadge from '../assets/google-play-badge.svg';
 import chartGPTLogo from '../assets/chatgpt-icon.svg';
 import bgPhone from '../assets/bg-phone.png';
+import ai from '../assets/ai.png';
 
 
 export {
     appStoreBadge,
     googlePlayBadge,
     chartGPTLogo,
-    bgPhone
+    bgPhone,
+    ai
 };
