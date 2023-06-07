06b4b1a6f08c223a8827d7af6eae68ec0d078721
felender <felender7@gmail.com>
2023-06-07 20:41:52 +0200

    added inputChange and addEmployee functions

--- REACT/task2/employee_management/src/components/EmployeeManagement.jsx
+++ REACT/task2/employee_management/src/components/EmployeeManagement.jsx
@@ -1,6 +1,7 @@
 import React, { useSate } from "react";
 
 function EmployeeManagement() {
+  //initialize employee
   const [employees, setEmployees] = useSate([]);
   const [newEmployee, setNewEmployee] = useSate({
     id_number: "",
@@ -12,7 +13,25 @@ function EmployeeManagement() {
     position: "",
   });
 
-  //
+  // handle input changes
+
+  function InputChange(event) {
+    const { firstName, value } = event.target;
+    setNewEmployee((prevEmployee) => ({ ...prevEmployee, [firstName]: value }));
+  }
+
+  function AddEmployee() {
+    setEmployees((prevEmployee) => [...prevEmployee, newEmployee]);
+    setEmployees({
+      id_number: "",
+      firstName: "",
+      lastName: "",
+      emailAddress: "",
+      phoneNumber: "",
+      image: "",
+      position: "",
+    });
+  }
 
   return <div></div>;
 }
