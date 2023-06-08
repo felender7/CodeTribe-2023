beac806daae8a04aeaa7859522a83849fe6f241e
felender <felender7@gmail.com>
2023-06-08 07:16:31 +0200

    added table under employee form

--- REACT/task2/employee_management/src/components/EmployeeManagement.jsx
+++ REACT/task2/employee_management/src/components/EmployeeManagement.jsx
@@ -36,8 +36,12 @@ function EmployeeManagement() {
       image: "",
     });
     if (newEmployee.idNumber !== "") {
-      document.getElementById("status").innerHTML = "Saved";
+      document.getElementById("status").innerHTML =
+        "Employee details saved successfully";
       console.log(newEmployee);
+    } else {
+      document.getElementById("status").innerHTML =
+        "Error saving some fields are missing";
     }
   }
 
@@ -81,115 +85,143 @@ function EmployeeManagement() {
       </div>
       <hr />
       <div className="row">
+        <span className="alert alert-primary" role="alert">
+          <label id="status"></label>
+        </span>
         <div className="col-md-6">
-            <h4>Add Employee</h4>
-            <hr />
-      <div className="mb-3">
-        <input
-          className="form-control"
-          type="text"
-          name="idNumber"
-          placeholder="ID Number"
-          value={newEmployee.idNumber}
-          onChange={InputChange}
-        />
-      </div>
-      <div className="mb-3">
-        <input
-          className="form-control"
-          type="text"
-          name="firstName"
-          placeholder="First Name"
-          value={newEmployee.firstName}
-          onChange={InputChange}
-        />
-      </div>
-      <div className="mb-3">
-        <input
-          type="text"
-          className="form-control"
-          name="lastName"
-          placeholder="Last Name"
-          value={newEmployee.lastName}
-          onChange={InputChange}
-        />
-      </div>
+          <h4>Add Employee</h4>
+          <hr />
+          <div className="mb-3">
+            <input
+              className="form-control"
+              type="text"
+              name="idNumber"
+              placeholder="ID Number"
+              value={newEmployee.idNumber}
+              onChange={InputChange}
+              required
+              id="validationCustom01"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              className="form-control"
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              value={newEmployee.firstName}
+              onChange={InputChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              name="lastName"
+              placeholder="Last Name"
+              value={newEmployee.lastName}
+              onChange={InputChange}
+              required
+            />
+          </div>
 
-      <div className="mb-3">
-        <input
-          className="form-control"
-          type="email"
-          name="emailAddress"
-          placeholder="Email Address"
-          value={newEmployee.emailAddress}
-          onChange={InputChange}
-        />
-      </div>
+          <div className="mb-3">
+            <input
+              className="form-control"
+              type="email"
+              name="emailAddress"
+              placeholder="Email Address"
+              value={newEmployee.emailAddress}
+              onChange={InputChange}
+              required
+            />
+          </div>
 
-      <div className="mb-3">
-        <input
-          className="form-control"
-          type="text"
-          name="phoneNumber"
-          placeholder="Phone Number"
-          value={newEmployee.phoneNumber}
-          onChange={InputChange}
-        />
-      </div>
-      <div className="mb-3">
-        <input
-          className="form-control"
-          type="text"
-          name="position"
-          placeholder="position"
-          value={newEmployee.position}
-          onChange={InputChange}
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <input
-          className="form-control"
-          type="file"
-          name="image"
-          placeholder="upload.."
-          value={newEmployee.image}
-          onChange={InputChange}
-          required
-        />
-      </div>
+          <div className="mb-3">
+            <input
+              className="form-control"
+              type="text"
+              name="phoneNumber"
+              placeholder="Phone Number"
+              value={newEmployee.phoneNumber}
+              onChange={InputChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              className="form-control"
+              type="text"
+              name="position"
+              placeholder="position"
+              value={newEmployee.position}
+              onChange={InputChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              className="form-control"
+              type="file"
+              name="image"
+              placeholder="upload.."
+              value={newEmployee.image}
+              onChange={InputChange}
+              required
+            />
+          </div>
 
-      <button className="btn btn-primary" onClick={AddEmployee}>
-        Add
-      </button>
-      <label id="status"></label>
-      <hr />
-      </div>
- 
+          <button className="btn btn-primary" onClick={AddEmployee}>
+            Add
+          </button>
+
+          <hr />
+        </div>
+
         <div className="col-md-6">
-        <h4>Employees</h4>
-        <hr />
-        {employees.map((employee) => (
-          <div key={employee.idNumber}>
-            <span>ID: {employee.idNumber}</span>
-            <span>Name: {employee.firstName}</span>
-            <button onClick={() => DeleteEmployee(employee.idNumber)}>
-              Delete
-            </button>
-            <button
-              onClick={() =>
-                UpdateEmployee(employee.idNumber, {
-                  ...employee,
-                  idNumber: "",
-                })
-              }
-            >
-              Update
-            </button>
-            </div>
-       
-        ))}
-      </div>
+          <h4>Employees</h4>
+          <hr />
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+              </tr>
+            </thead>
+            {employees.map((employee) => (
+              <div key={employee.idNumber}>
+                <tbody>
+                  <tr>
+                    <td>{employee.idNumber}</td>
+                    <td>{employee.firstName}</td>
+                    <td>{employee.lastName}</td>
+                    <td>{employee.emailAddress}</td>
+                  </tr>
+                </tbody>
+                <button
+                  onClick={() => DeleteEmployee(employee.idNumber)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+                <button
+                  className="btn btn-success btn-sm"
+                  onClick={() =>
+                    UpdateEmployee(employee.idNumber, {
+                      ...employee,
+                      idNumber: "",
+                    })
+                  }
+                >
+                  Update
+                </button>
+              </div>
+            ))}
+          </table>
+        </div>
       </div>
     </div>
   );
