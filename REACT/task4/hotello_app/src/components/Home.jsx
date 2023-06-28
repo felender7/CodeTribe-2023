afd349fe39994c6cfd3d7253974efcff2075cb0c
felender7 <felender7@gmail.com>
2023-06-28 21:23:25 +0200

    added about component

--- REACT/task4/hotello_app/src/components/Home.jsx
+++ REACT/task4/hotello_app/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from './subcomponents/Navbar'
 import Banner from "./subcomponents/Banner"
+import AboutSection from "./subcomponents/AboutSection";
 
 function Home() {
   return (
     <div>
         <Navbar/>
         <Banner/>
+        <AboutSection/>
     </div>
   )
 }
