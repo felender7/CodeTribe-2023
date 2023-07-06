93be66b1279723de3fc26d23f8c3f857929dbaa3
felender7 <felender7@gmail.com>
2023-07-06 12:49:48 +0200

    added about and room page

--- REACT/task4/hotello_app/src/components/Imports.jsx
+++ REACT/task4/hotello_app/src/components/Imports.jsx
@@ -7,7 +7,9 @@ import Room1 from "../assets/room1.jpeg"
 import Room2 from "../assets/room2.jpeg"
 import Room3 from "../assets/room3.jpg"
 import Room4 from "../assets/room4.jpeg"
-
+import Home1 from '../assets/home_image_1.jpg'
+import Home2 from '../assets/home_image_2.jpg'
+import Home3 from '../assets/home_image_3.jpg'
 
 
 
@@ -22,6 +24,9 @@ export{
     Room1,
     Room2,
     Room3,
-    Room4
+    Room4,
+    Home1,
+    Home2,
+    Home3,
    
 }
--- REACT/task4/hotello_app/src/components/Rooms.jsx
+++ REACT/task4/hotello_app/src/components/Rooms.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Navbar from './subcomponents/Navbar'
+import Footer from './subcomponents/Footer'
+import HotelListing from './subcomponents/HotelListing'
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../config/firebase"
 
 function Rooms() {
+  const [hotels, setHotels] = useState([]);
+  useEffect(() => {
+    const fetchHotels = async () => {
+      try {
+        // Retrieve the collection "hotels" from Firestore
+        const querySnapshot = await getDocs(collection(db, "hotels"));
+
+        // Map the query snapshot to an array of hotel data objects
+        const hotelsData = querySnapshot.docs.map((doc) => doc.data());
+        console.log(hotelsData)
+
+
+        // Set the hotels state with the retrieved data
+        setHotels(hotelsData);
+      } catch (error) {
+        console.error("Error fetching hotels: ", error);
+      }
+    };
+
+    fetchHotels();
+  }, []);
+
+
+
   return (
     <div>
-      <h1>Home</h1>
+      <Navbar />
+      <div class="banner">
+        <div>
+          <h1 className='text-light'>Rooms & suits</h1>
+        </div>
+      </div>
+
+      <HotelListing hotels={hotels} />
+      <Footer />
     </div>
   )
 }
