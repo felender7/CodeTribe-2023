58c0313379a48c0b73343479a380042de8374580
felender7 <felender7@gmail.com>
2023-07-12 09:02:45 +0200

    added map on and formspree api

--- REACT/task4/hotello_app/src/components/Contacts.jsx
+++ REACT/task4/hotello_app/src/components/Contacts.jsx
@@ -5,7 +5,7 @@ import Footer from './subcomponents/Footer'
 function Contacts() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <div class="banner">
         <div>
           <h1 className='text-light'>Get in touch</h1>
@@ -13,34 +13,36 @@ function Contacts() {
       </div>
 
 
-    <section class="container py-5">
+      <section class="container py-5">
         <p>Waters midst. Creature man female. Them You’re female there his for first were have whose and blessed darkness his man Darkness had you’re hath fill were. Gathering may living moveth had evening called behold. Blessed darkness subdue he open she’d of void sea one let after light thing have creeping living the brought</p>
-    <div class="row">
-      <div class="col-lg-6 mt-5">
-  
-        <form action="https://formspree.io/f/xzblbkll" method="post">
-          <div class="mb-3">
-            <input type="text" class="form-control" id="name" placeholder="Name" required/>
+        <div class="row">
+          <div class="col-lg-6 mt-5">
+
+            <form action="https://formspree.io/f/xzblbkll" method="post">
+              <div class="mb-3">
+                <input type="text" class="form-control" id="name" placeholder="Name" required />
+              </div>
+              <div class="mb-3">
+                <input type="email" class="form-control" id="email" placeholder='Email' required />
+              </div>
+              <div class="mb-3">
+                <textarea class="form-control" id="message" rows="5" required placeholder='Message'></textarea>
+              </div>
+              <button type="submit" class="btn btn-outline-success">Submit</button>
+            </form>
           </div>
-          <div class="mb-3">
-            <input type="email" class="form-control" id="email" placeholder='Email' required/>
+          <div class="col-lg-6 mt-5">
+            <p>28 Doornkraal, Annadale <br />Polokwane<br /> 0699</p>
+            <div class="map">
+            <iframe
+            src="https://www.google.com/maps?q=28%20Doornkraal%20Annandale,%20Polokwane&amp;output=embed"
+            width="100%" height="300" frameborder="0" style={{border:"0"}} allowfullscreen="" aria-hidden="false"
+            tabindex="0"></iframe>
+            </div>
           </div>
-          <div class="mb-3">
-            <textarea class="form-control" id="message" rows="5" required placeholder='Message'></textarea>
-          </div>
-          <button type="submit" class="btn btn-outline-success">Submit</button>
-        </form>
-      </div>
-      <div class="col-lg-6 mt-5">
-        <h2>Our Location</h2>
-        <p>28 Doornkraal, Annadale</p>
-        <div class="map">
-       
         </div>
-      </div>
-    </div>
-  </section>
-      <Footer/>
+      </section>
+      <Footer />
     </div>
   )
 }
